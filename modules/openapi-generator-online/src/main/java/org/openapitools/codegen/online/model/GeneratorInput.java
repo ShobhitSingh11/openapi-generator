7c31de9cbe949f8c7cd8cbbfae67dc2061da504a
ShobhitSingh11 <139750384+ShobhitSingh11@users.noreply.github.com>
2024-02-02 13:37:07 +0530

    Update GeneratorInput.java

--- modules/openapi-generator-online/src/main/java/org/openapitools/codegen/online/model/GeneratorInput.java
+++ modules/openapi-generator-online/src/main/java/org/openapitools/codegen/online/model/GeneratorInput.java
@@ -28,7 +28,7 @@ public class GeneratorInput {
     private Map<String, String> options;
     private String openAPIUrl;
     private AuthorizationValue authorizationValue;
-
+    // efbjerhfgjer
     public AuthorizationValue getAuthorizationValue() {
         return authorizationValue;
     }
