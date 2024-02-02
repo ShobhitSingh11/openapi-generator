dc48ff78ffc3952fc7cc2636bf547c88992ce5a0
ShobhitSingh11 <139750384+ShobhitSingh11@users.noreply.github.com>
2024-02-02 14:04:22 +0530

    Update CliOption.java

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/CliOption.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/CliOption.java
@@ -25,7 +25,7 @@ import java.util.Map;
 import java.util.Objects;
 
 public class CliOption {
-    // vg
+    // vgfdevrfvfr
     private final String opt;
     private String description;
     private String type;
