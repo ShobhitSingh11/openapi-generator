14aeec12b2b9916ccebc736fd6d7fda2ec87ef75
ShobhitSingh11 <139750384+ShobhitSingh11@users.noreply.github.com>
2024-02-22 22:09:08 +0530

    Update DefaultCodegen.java

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/DefaultCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/DefaultCodegen.java
@@ -86,7 +86,7 @@ import static org.openapitools.codegen.utils.StringUtils.*;
 
 public class DefaultCodegen implements CodegenConfig {
     private final Logger LOGGER = LoggerFactory.getLogger(DefaultCodegen.class);
-
+    // frrfrfrefrefrefrefedfe
     public static FeatureSet DefaultFeatureSet;
 
     // A cache of sanitized words. The sanitizeName() method is invoked many times with the same
