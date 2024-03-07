5a09d089b513915556a3f8b9d29f22ab02527e2e
ShobhitSingh11 <139750384+ShobhitSingh11@users.noreply.github.com>
2024-03-07 13:44:03 +0530

    Update Constants.java

--- modules/openapi-generator-cli/src/main/java/org/openapitools/codegen/Constants.java
+++ modules/openapi-generator-cli/src/main/java/org/openapitools/codegen/Constants.java
@@ -2,7 +2,7 @@ package org.openapitools.codegen;
 
 public class Constants {
     private Constants(){ }
-
+    // eferferfdewfewfewfer3
     public static final String CLI_NAME = "openapi-generator-cli";
     public static final String GIT_REPO = "https://github.com/openapitools/openapi-generator";
     public static final String SITE = "https://openapi-generator.tech/";
