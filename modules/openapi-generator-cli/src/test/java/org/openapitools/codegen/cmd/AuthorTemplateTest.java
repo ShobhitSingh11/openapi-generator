8ac13200248437e7854ad8a9be782e66066efa56
ShobhitSingh11 <139750384+ShobhitSingh11@users.noreply.github.com>
2024-03-25 23:31:18 +0530

    Update AuthorTemplateTest.java

--- modules/openapi-generator-cli/src/test/java/org/openapitools/codegen/cmd/AuthorTemplateTest.java
+++ modules/openapi-generator-cli/src/test/java/org/openapitools/codegen/cmd/AuthorTemplateTest.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
-
+//ref
 public class AuthorTemplateTest {
     Path outputDirectory;
 
